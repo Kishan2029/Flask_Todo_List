eefa3832458eb6d8acdcad9170eb15c5e823a2eb
kishan-2029 <kishan.prajapati@searce.com>
2023-06-26 22:11:44 +0530

    requirement file added

--- app.py
+++ app.py
@@ -72,4 +72,4 @@ def update(id):
 #         db.create_all()
 #         app.run(debug=True)
 if (__name__ == "__main__"):
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=4000, debug=True)
