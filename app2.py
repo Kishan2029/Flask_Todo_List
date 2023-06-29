59fb51554de440bcf74d1303645c6e92329d7bb3
kishan-2029 <kishan.prajapati@searce.com>
2023-06-29 16:38:00 +0530

    requirement file added

--- app2.py
+++ app2.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from waitress import serve
 
 app = Flask(__name__)
 app.app_context().push()
